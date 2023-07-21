68e5f42be33d0d7b72f75758076ce976c5f4934a
jiang <jiang762507357@163.com>
2023-07-21 15:49:45 +0800

    optimize log

--- readiness.go
+++ readiness.go
@@ -6,8 +6,6 @@ import (
 	"net/url"
 	"sync/atomic"
 
-	"github.com/openfaas/of-watchdog/logger"
-
 	limiter "github.com/openfaas/faas-middleware/concurrency-limiter"
 )
 
@@ -32,7 +30,7 @@ func (r *readiness) LimitMet() bool {
 }
 
 func (r *readiness) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	logger.Info("http req method", "method", req.Method, "scheme", req.URL.Scheme, "url", req.URL.Host, "endpoint", r.endpoint)
+	log.Printf("http req method,method:%v, scheme:%v, url:%v, endpoint: %v \n", req.Method, req.URL.Scheme, req.URL.Host, r.endpoint)
 	switch req.Method {
 	case http.MethodGet:
 		status := http.StatusOK
